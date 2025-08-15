afb1c12e962f6c1b752845aa71f64b9daddab4c6
muhammad <m.abduraimov@agrobank.uz>
2025-08-15 23:18:22 +0500

    auth

---
+++ notification/src/main/java/com/example/notification/NotificationApplication.java
@@ -0,0 +1,16 @@
+package com.example.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
